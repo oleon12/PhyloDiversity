585a317cc44b81503cc1c3f4f3bcd6330cf77c8f
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-02-10 03:23:31 -0500

    Update Absecen.Presence_Matrix.R

--- Absecen.Presence_Matrix.R
+++ Absecen.Presence_Matrix.R
@@ -15,18 +15,18 @@ library(doParallel)
 
 ## Set thw working directory.
 
-setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Genes/Mammals/")
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Aves/Results/Ene8/")
 
 ## Read the occurences data.
-sp.dist <- read.table("Mammals.occ",sep = " ",header=T)
+sp.dist <- read.csv("Dist_ene8.occ",header = T)
 # Create a new data frame pasting the two first original columns.
-sp.dist <- data.frame(Sp=paste(sp.dist[,1],sp.dist[,2],sep="_"), 
-                   Lon=sp.dist$Lon,
-                   Lat=sp.dist$Lat)
+#sp.dist <- data.frame(Sp=paste(sp.dist[,1],sp.dist[,2],sep="_"), 
+#                      Lon=sp.dist$Lon,
+#                      Lat=sp.dist$Lat)
 # Write the new table.
-write.csv(data,"mammals2.occ",row.names=F,col.names=T,quote=F) 
+#write.csv(data,"mammals2.occ",row.names=F,col.names=T,quote=F) 
 
-sp.dist
+head(sp.dist,3L)
 
 ## Create an empty list object.
 area <- list() 
@@ -43,15 +43,15 @@ for(i in 1:length(area.dir)){
 
 # Create a matrix filled with 0, the rows will be the species and the columns will be the areas.
 # This will be our absece/presence matrix.
-data.abpre <- matrix(0,nrow = length(levels(sp.dist$Sp)),ncol=length(area))
+data.abpre <- matrix(0,nrow = length(levels(sp.dist$especie)),ncol=length(area))
 colnames(data.abpre) <- area.dir
-rownames(data.abpre) <- levels(sp.dist$Sp)
+rownames(data.abpre) <- levels(sp.dist$especie)
 
 # Now we need to fill the matrix with the presences.
 for(i in 1:length(area)){
   for(j in 1:length(data.abpre[,1])){
     # Extrac only the Lat and Long for a j specie given the data.abpre matrix.
-    sp.data <- sp.dist[which(sp.dist$Sp==rownames(data.abpre)[j]),2:3]
+    sp.data <- sp.dist[which(sp.dist$especie==rownames(data.abpre)[j]),2:3]
     # Compare if at least one georeference point are inside the area polygon.
     out <- pnt.in.poly(sp.data,area[[i]]$shp[[1]]$points)
     # If a point are inside the area polygon, will fill his respective space with 1.
@@ -61,7 +61,7 @@ for(i in 1:length(area)){
 
 # Save the matrix in his respective directory.
 
-setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Genes/Mammals/")
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Aves/Results/Ene8/")
 write.table(data.abpre,file="Area.dist.matrix",row.names=T, col.names=T,quote=F,sep=",")
 
 
@@ -70,14 +70,14 @@ write.table(data.abpre,file="Area.dist.matrix",row.names=T, col.names=T,quote=F,
 # Read the grid from his respective directory.
 
 setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Distribution/shp/Grid/")
-grid.g <- read.shp("grid_50g.shp")
+grid.g <- read.shp("grid_1g.shp")
 
 # Create a matrix filled with 0, the rows will be the species and the columns will be the areas.
 # This will be our absece/presence matrix.
 
-data.grid <- matrix(0, nrow = length(levels(sp.dist$Sp)),ncol = length(grid.g$shp))
+data.grid <- matrix(0, nrow = length(levels(sp.dist$especie)),ncol = length(grid.g$shp))
 colnames(data.grid) <- 1:length(grid.g$shp)
-rownames(data.grid) <- levels(sp.dist$Sp)
+rownames(data.grid) <- levels(sp.dist$especie)
 
 cl <- makeCluster(8)
 registerDoParallel(cl)
@@ -87,7 +87,7 @@ for(i in 1:length(data.grid[,1])){
   print(i) # Checkpoint
   for (j in 1:length(grid.g$shp)){
     # Extrac only the Lat and Long for a j specie given the data.grid matrix.
-    sp.data <- sp.dist[which(sp.dist$Sp==rownames(data.grid)[i]),2:3]
+    sp.data <- sp.dist[which(sp.dist$especie==rownames(data.grid)[i]),2:3]
     # Compare if at least one georeference point are inside the area polygon.
     out <- pnt.in.poly(sp.data,grid.g$shp[[1]]$points)
     # If a point are inside the area polygon, will fill his respective space with 1.
@@ -96,5 +96,43 @@ for(i in 1:length(data.grid[,1])){
 }
 
 # Save the created data.
-setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Genes/Mammals/")
-write.table(data.grid,file="grid_25g.dist.matrix",row.names=T, col.names=T,quote=F,sep=",")
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Aves/Results/Ene8/")
+write.table(data.grid,file="grid_1g.dist.matrix",row.names=T, col.names=T,quote=F,sep=",")
+
+
+# Read the PNN shp file from his respective directory.
+
+setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Distribution/shp/PNN/")
+dir <- dir()[grep(".shp",dir())]
+PNN <- list()
+
+for(i in 1:length(dir)){
+  PNN[[i]] <- read.shp(shp.name = dir[i])
+}
+
+# Create a matrix filled with 0, the rows will be the species and the columns will be the areas.
+# This will be our absece/presence matrix.
+
+data.PNN <- matrix(0, nrow = length(levels(sp.dist$especie)),ncol = length(PNN))
+colnames(data.PNN) <- dir
+rownames(data.PNN) <- levels(sp.dist$especie)
+
+cl <- makeCluster(8)
+registerDoParallel(cl)
+
+for(i in 1:length(PNN)){
+  print(paste("Total PNN:",length(PNN),sep=""))
+  print(dir[i])
+  for(j in 1:length(data.PNN[,1])){
+    # Extrac only the Lat and Long for a j specie given the data.abpre matrix.
+    sp.data <- sp.dist[which(sp.dist$especie==rownames(data.PNN)[j]),2:3]
+    # Compare if at least one georeference point are inside the area polygon.
+    out <- pnt.in.poly(sp.data,PNN[[i]]$shp[[1]]$points)
+    # If a point are inside the area polygon, will fill his respective space with 1.
+    if (1%in%out$pip){ data.PNN[j,i]<- 1} 
+  }
+}
+
+# Save the created data.
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Aves/Results/Ene8/")
+write.table(data.grid,file="PNN.dist.matrix",row.names=T, col.names=T,quote=F,sep=",")
