b108cbaf9548a517d89d65fa5d827fc728ce03bf
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-05-24 18:07:48 -0500

    Update Absence.Presence_Matrix.R

--- Absence.Presence_Matrix.R
+++ Absence.Presence_Matrix.R
@@ -34,10 +34,10 @@ library(doParallel)
 
 ## Set thw working directory.
 
-setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Aves/Results/Ene8/")
+setwd("~/Documentos/Omar/Tesis/Taxa/Richness/")
 
 ## Read the occurences data.
-sp.dist <- read.csv("Dist_ene8.occ",header = T)
+sp.dist <- read.csv("Richness.occ",header = T)
 # Create a new data frame pasting the two first original columns.
 #sp.dist <- data.frame(Sp=paste(sp.dist[,1],sp.dist[,2],sep="_"), 
 #                      Lon=sp.dist$Lon,
@@ -62,15 +62,15 @@ for(i in 1:length(area.dir)){
 
 # Create a matrix filled with 0, the rows will be the species and the columns will be the areas.
 # This will be our absece/presence matrix.
-data.abpre <- matrix(0,nrow = length(levels(sp.dist$especie)),ncol=length(area))
+data.abpre <- matrix(0,nrow = length(levels(sp.dist$Sp)),ncol=length(area))
 colnames(data.abpre) <- area.dir
-rownames(data.abpre) <- levels(sp.dist$especie)
+rownames(data.abpre) <- levels(sp.dist$Sp)
 
 # Now we need to fill the matrix with the presences.
 for(i in 1:length(area)){
   for(j in 1:length(data.abpre[,1])){
     # Extrac only the Lat and Long for a j specie given the data.abpre matrix.
-    sp.data <- sp.dist[which(sp.dist$especie==rownames(data.abpre)[j]),2:3]
+    sp.data <- sp.dist[which(sp.dist$Sp==rownames(data.abpre)[j]),2:3]
     # Compare if at least one georeference point are inside the area polygon.
     out <- pnt.in.poly(sp.data,area[[i]]$shp[[1]]$points)
     # If a point are inside the area polygon, will fill his respective space with 1.
@@ -80,7 +80,7 @@ for(i in 1:length(area)){
 
 # Save the matrix in his respective directory.
 
-setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Aves/Results/Ene8/")
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/May18/")
 write.table(data.abpre,file="Area.dist.matrix",row.names=T, col.names=T,quote=F,sep=",")
 
 
@@ -89,14 +89,14 @@ write.table(data.abpre,file="Area.dist.matrix",row.names=T, col.names=T,quote=F,
 # Read the grid from his respective directory.
 
 setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Distribution/shp/Grid/")
-grid.g <- read.shp("grid_50g.shp")
+grid.g <- read.shp("grid_25g.shp")
 
 # Create a matrix filled with 0, the rows will be the species and the columns will be the areas.
 # This will be our absece/presence matrix.
 
-data.grid <- matrix(0, nrow = length(levels(sp.dist$especie)),ncol = length(grid.g$shp))
+data.grid <- matrix(0, nrow = length(levels(sp.dist$Sp)),ncol = length(grid.g$shp))
 colnames(data.grid) <- 1:length(grid.g$shp)
-rownames(data.grid) <- levels(sp.dist$especie)
+rownames(data.grid) <- levels(sp.dist$Sp)
 
 cl <- makeCluster(8)
 registerDoParallel(cl)
@@ -106,7 +106,7 @@ for(i in 1:length(data.grid[,1])){
   print(paste("Specie:",i,sep="")) # Checkpoint
   for (j in 1:length(grid.g$shp)){
     # Extrac only the Lat and Long for a j specie given the data.grid matrix.
-    sp.data <- sp.dist[which(sp.dist$especie==rownames(data.grid)[i]),2:3]
+    sp.data <- sp.dist[which(sp.dist$Sp==rownames(data.grid)[i]),2:3]
     # Compare if at least one georeference point are inside the area polygon.
     out <- pnt.in.poly(sp.data,grid.g$shp[[j]]$points)
     # If a point are inside the area polygon, will fill his respective space with 1.
@@ -114,9 +114,11 @@ for(i in 1:length(data.grid[,1])){
   }
 }
 
+stopCluster(cl)
+
 # Save the created data.
-setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Aves/Results/Ene8/")
-write.table(data.grid,file="grid_50g.dist.matrix",row.names=T, col.names=T,quote=F,sep=",")
+setwd("~/Documentos/Omar/Tesis/Taxa/Results/May18/")
+write.table(data.grid,file="grid_25g.dist.matrix",row.names=T, col.names=T,quote=F,sep=",")
 
 
 # Read the PNN shp file from his respective directory.
@@ -129,29 +131,69 @@ for(i in 1:length(dir)){
   PNN[[i]] <- read.shp(shp.name = dir[i])
 }
 
+
+setwd("~/Documentos/Omar/Tesis/Scripts/Distribution/shp/NAB/")
+NAB <- read.shp("NAB_GBIF.shp")
+
 # Create a matrix filled with 0, the rows will be the species and the columns will be the areas.
 # This will be our absece/presence matrix.
 
-data.PNN <- matrix(0, nrow = length(levels(sp.dist$especie)),ncol = length(PNN))
+data.PNN <- matrix(0, nrow = length(levels(sp.dist$Sp)),ncol = length(PNN))
 colnames(data.PNN) <- dir
-rownames(data.PNN) <- levels(sp.dist$especie)
+rownames(data.PNN) <- levels(sp.dist$Sp)
 
+## Now other approach, evaluating only if the species are inside PNNs or not
+## Also, the NAB shape will use to next calculate the total Phylogenetic diversity of whole NAB
+
+
+NABin <- rep(1,length(levels(sp.dist$Sp)))
+PNNin <- rep(0,length(levels(sp.dist$Sp)))
+PNNout <- rep(0,length(levels(sp.dist$Sp)))
+
+
+## Calculation for each PNN
+##############################################################################
 cl <- makeCluster(8)
 registerDoParallel(cl)
 
 for(i in 1:length(PNN)){
-  print(paste("Total PNN:",length(PNN),sep=""))
-  print(paste("PNN #:",i,sep=""))
+  print(paste("Calculating...",(length(PNN):1)[i],sep = " "))
   for(j in 1:length(data.PNN[,1])){
     # Extrac only the Lat and Long for a j specie given the data.abpre matrix.
-    sp.data <- sp.dist[which(sp.dist$especie==rownames(data.PNN)[j]),2:3]
+    sp.data <- sp.dist[which(sp.dist$Sp==rownames(data.PNN)[j]),2:3]
     # Compare if at least one georeference point are inside the area polygon.
     out <- pnt.in.poly(sp.data,PNN[[i]]$shp[[1]]$points)
     # If a point are inside the area polygon, will fill his respective space with 1.
-    if (1%in%out$pip){ data.PNN[j,i]<- 1; print(i)} 
+    if (1%in%out$pip){ 
+      
+      data.PNN[j,i]<- 1
+    }
   }
 }
 
+stopImplicitCluster()
+#######################################################################
+
+for(i in 1:length(data.PNN[,1])){
+  
+  ll <- which(data.PNN[i,]==1)
+  
+  if(length(ll)>=1){
+    
+    PNNin[i] <- 1
+  
+  }else{
+    
+    PNNout[i] <- 1
+    
+  }
+  
+}
+
+########################################################################
+data.PNN <- cbind(data.PNN,PNNin,PNNout,NABin)
+########################################################################
+
 # Save the created data.
-setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Aves/Results/Ene8/")
-write.table(data.PNN,file="PNN.dist.matrix",row.names=T, col.names=T,quote=F,sep=",")
+setwd("~/Documentos/Omar/Tesis/Taxa/Results/May18/")
+write.table(data.PNN,file="PNN.dist.matrix",row.names=T, col.names=T,quote=F,sep="|")
