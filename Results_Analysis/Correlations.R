7a5db1702843a39373dc386dbcc55733d5253641
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-06-05 20:54:13 -0500

    Update Correlations.R

--- Results_Analysis/Correlations.R
+++ Results_Analysis/Correlations.R
@@ -1,43 +1,53 @@
 library(ggplot2)
 
 ## Set working directory
-setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Richness/")
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/May18/")
 
 ## Read the absence/presence tables for the total richness
-area.rich <- read.csv("Area.rich")
-grid.rich <- read.csv("grid_25g.rich")
+
+area.rich <- read.csv("Area.dist.matrix")
+grid.rich <- read.csv("grid_25g.dist.matrix")
+
 ## Create two vectors where the total richness will are
+
 area.t.rich <- c()
 grid.t.rich <- c()
+
 ## Make the summatory of the richness (This could be made by a apply function, check it later)
-for(i in 1:length(colnames(area.rich))){
+
+for(i in 2:length(colnames(area.rich))){
   total <- sum(area.rich[,i])
   area.t.rich[i] <- total
 }
 
-for(i in 1:length(colnames(grid.rich))){
+for(i in 2:length(colnames(grid.rich))){
   total <- sum(grid.rich[,i])
   grid.t.rich[i] <- total
 }
+
+
+area.t.rich <- area.t.rich[-1]
+
+grid.t.rich <- grid.t.rich[-1]
 
 # Create the two data frames with the index values and richness values
 # f.Rich are the the filogenetic richness
 # t.Rich are the whole richness
 
-setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/Abril1/")
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/May18/Raw_IndexR/")
 
-index.area <- data.frame(Area=read.csv("DTenar.total")$area,
-                         DT=read.csv("DTenar.total")$W,
-                         PD=read.csv("PDenar.total")$PD,
-                         AvDT=read.csv("AVDTenar.total")$Dplus,
-                         f.Rich=read.csv("DTenar.total")$rich,
+index.area <- data.frame(Area=read.csv("DT.Area")$area,
+                         DT=read.csv("DT.Area")$W,
+                         PD=read.csv("PD.Area")$PD,
+                         AvDT=read.csv("AvTD.Area")$Dplus,
+                         f.Rich=read.csv("DT.Area")$rich,
                          t.Rich=area.t.rich)
 
-index.grid <- data.frame(Cell.Grid=read.csv("DTg25.total")$area,
-                         DT=read.csv("DTg25.total")$W,
-                         PD=read.csv("PDg25.total")$PD,
-                         AvDT=read.csv("AVDTg25.total")$Dplus,
-                         f.Rich=read.csv("DTg25.total")$rich,
+index.grid <- data.frame(Cell.Grid=read.csv("DT.grid25")$area,
+                         DT=read.csv("DT.grid25")$W,
+                         PD=read.csv("PD.grid25")$PD,
+                         AvDT=read.csv("AvTD.grid25")$Dplus,
+                         f.Rich=read.csv("DT.grid25")$rich,
                          t.Rich=grid.t.rich)
 
 ## Check first f.Rich vs t.Rich
@@ -103,8 +113,10 @@ correlations[2,3] <- avdt$estimate
 
 correlations
 
-write.table(correlations,file="Corr.RichvIndex",quote = F, row.names = T,col.names = T)
+write.table(correlations,file="~/Documentos/Omar/Tesis/Taxa/Results/May18/Corr.RichvIndex",
+            quote = F, row.names = T,col.names = T)
 
 ind.cor <- cor(index.grid[,2:4],method = "spearman")
 
-write.table(ind.cor,file = "Corr.IndvInd", quote = F, row.names = T,col.names = T)
+write.table(ind.cor,file = "~/Documentos/Omar/Tesis/Taxa/Results/May18/Corr.IndvInd", 
+            quote = F, row.names = T,col.names = T)
