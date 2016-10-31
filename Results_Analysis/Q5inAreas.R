71c1d2d1c071238d07be77596c9965e7f999964f
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-10-31 01:07:59 -0500

    Update Q5inAreas.R

--- Results_Analysis/Q5inAreas.R
+++ Results_Analysis/Q5inAreas.R
@@ -10,9 +10,9 @@ library(shapefiles)
 library(SDMTools)
 
 ## Go to the work directory where are the shape poly 
-setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/Julio1/RawIndex/")
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/Final/Raw_IndexR/")
 # Read the shape file
-grid <- readShapePoly("Grid25_PD.shp")
+grid <- readShapePoly("Grid25_AVTD.shp")
 pos.Q5 <- grep("Q5", grid$Index)
 pos.Q5 <- pos.Q5 + 1
 #
@@ -33,7 +33,7 @@ for(i in 1:length(pos.Q5)){
   cell <- grid2$shp[[pos]]$points
   
   for (j in 1:length(PNN.names)) {
-  
+    
     PNN.coord <- PNN$shp[[j]]$points
     plot(PNN.coord)
     
@@ -47,9 +47,15 @@ for(i in 1:length(pos.Q5)){
       plot(cell)
     }
     
+    if(length(n)==4){
+      
+      CellPerc[1,j] <- (CellPerc[1,j]+1)
+      plot(cell)
+    }
+    
     if(length(n)==3){
       
-     CellPerc[1,j] <- (CellPerc[1,j]+0.75)
+      CellPerc[1,j] <- (CellPerc[1,j]+0.75)
       
     }
     
@@ -72,4 +78,5 @@ CellPerc
 
 sum(CellPerc)/length(pos.Q5)
 
-(CellPerc[,7]+CellPerc[,11])/length(pos.Q5)
+(CellPerc[,6]+CellPerc[,10])/length(pos.Q5)
+
