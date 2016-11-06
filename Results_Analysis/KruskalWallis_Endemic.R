b3abd215c1ec54a7a697b1ec24e5f1edaf6c2e36
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-11-06 00:30:59 -0500

    Update KruskalWallis_Endemic.R

--- Results_Analysis/KruskalWallis_Endemic.R
+++ Results_Analysis/KruskalWallis_Endemic.R
@@ -1,26 +1,107 @@
-setwd("~/Documentos/Omar/Tesis/Taxa/Results/Julio1/Endemic/")
+setwd("~/Documentos/Omar/Tesis/Taxa/Results/Final/Endemic/")
 
-end.table <- read.csv("Grid.end")
+#################################################################################################
+##                                                                                             ##
+##                                   Grido of 0.25° Cells                                      ##
+##                                                                                             ##
+#################################################################################################
 
-head(end.table)
+############################################################
+# TD
+#
+
+end<- read.csv("Grid.end")
+
+head(end)
+
+class <- readShapePoly("~/Documentos/Omar/Tesis/Taxa/Results/Final/Raw_IndexR/Grid25_TD.shp")
+
+ind <- class$Index
+
+ind <- rep(ind,(length(end$Area)/length(unique(end$Area))))
+           
+end <- cbind(end,ind)
+     
+q5 <- grep("Q5",end$ind)
+           
+q4 <- grep("Q4",end$ind)
+
+end2 <- end[-which(end$TD==0), ]
+
+kruskal.test(end2$TD[c(q5,q4)],as.factor(end2$Percentage[c(q5,q4)]))
+
+############################################################
+# PD
+#
+
+end<- read.csv("Grid.end")
+
+head(end)
+
+class <- readShapePoly("~/Documentos/Omar/Tesis/Taxa/Results/Final/Raw_IndexR/Grid25_PD.shp")
+
+ind <- class$Index
+
+ind <- rep(ind,(length(end$Area)/length(unique(end$Area))))
+
+end <- cbind(end,ind)
+
+q5 <- grep("Q5",end$ind)
+
+q4 <- grep("Q4",end$ind)
+
+end2 <- end[-which(end$PD==0), ]
+
+kruskal.test(end2$PD[c(q5,q4)],as.factor(end2$Percentage[c(q5,q4)]))
 
 ############################################################
 # AvTD
 #
-which(end.table$TD==0)
 
-kruskal.test(end.table$TD[-which(end.table$TD==0)],as.factor(end.table$Percentage[-which(end.table$TD==0)]))
+end<- read.csv("Grid.end")
+
+head(end)
+
+class <- readShapePoly("~/Documentos/Omar/Tesis/Taxa/Results/Final/Raw_IndexR/Grid25_AvTD.shp")
+
+ind <- class$Index
+
+ind <- rep(ind,(length(end$Area)/length(unique(end$Area))))
+
+end <- cbind(end,ind)
+
+q5 <- grep("Q5",end$ind)
+
+q4 <- grep("Q4",end$ind)
+
+end2 <- end[-which(end$AvTD==0), ]
+
+kruskal.test(end2$AvTD[c(q5,q4)],as.factor(end2$Percentage[c(q5,q4)]))
+
+#################################################################################################
+##                                                                                             ##
+##                                   Areas of endemism                                         ##
+##                                                                                             ##
+#################################################################################################
+
+############################################################
+# TD
+#
+
+end<- read.csv("Area.end")
+
+kruskal.test(end$TD,as.factor(end$Percentage))
+
+############################################################
+# PD
+#
+end<- read.csv("Area.end")
+
+kruskal.test(end$PD,as.factor(end$Percentage))
 
 ############################################################
 # AvTD
 #
-which(end.table$PD==0)
+end<- read.csv("Area.end")
 
-kruskal.test(end.table$PD[-which(end.table$PD==0)],as.factor(end.table$Percentage[-which(end.table$PD==0)]))
-
-############################################################
-# AvTD
-#
-which(end.table$AvTD==0)
-
-kruskal.test(end.table$AvTD[-which(end.table$AvTD==0)],as.factor(end.table$Percentage[-which(end.table$AvTD==0)]))
+kruskal.test(end$AvTD,as.factor(end$Percentage))
