b6eafc461d1944d417e63ae552c8a7a3f2dee83b
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2017-01-20 14:23:26 -0500

    Update RtoGIS.R

--- RtoGIS.R
+++ RtoGIS.R
@@ -10,20 +10,20 @@ library(dismo)
 ## Go to the work directory where are the shape poly 
 setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Distribution/shp/Grid/")
 # Read the shape file
-grid <- readShapePoly("grid_1g.shp")
+grid <- readShapePoly("grid_25g.shp")
 #
 # Now, go to the directory where are the index results
 setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/Final2/RawIndex_R/")
 # Read the results depending on the cell size
-x <- read.csv("AvTD.grid1",header=T)
+x <- read.csv("AvTD.grid25",header=T)
 # Extract the index values
-area <- read.csv("DT.grid1")$area 
+area <- read.csv("DT.grid25")$area 
 x <- x$Dplus
 # Check the values
 x
 # Due to there are many cell without index values (this is beacuse the species distribution)
 # remove the 0 values, or the quantile classification will be biased
-x2 <- x[-grep(0,x)]
+x2 <- x[which(x>0.0000000)]
 
 # No, do the quantile clasification, in this case, given the values index, the intervals will be generated
 brks <- classIntervals(x2,n=5,style = "quantile")
@@ -37,85 +37,159 @@ brks
 class <- findInterval(x,brks,all.inside = T)
 # Now, beacuase the focus is only in the quantile 5 and 4, (those with highest index values)
 # The cells out of the Q5 and Q4 will be replace with NO
-class[-c(grep(5,class),grep(4,class))] <-  "NO"
+Pos5 <- grep(5,class)
+Pos4 <- grep(4,class)
+Pos3 <- grep(3,class)
+Pos2 <- grep(2,class)
+NonVal <- which(x==0)
+
+class[-Pos5] <-  "Non-Q5"
 # Now, those cell int in the two last quantiles, will be filled with the quotes "Q5" and "Q4"
-class[grep(5,class)] <- "Q5"
-class[grep(4,class)] <- "Q4"
-# See the result
-class
-#
-# Now, the hard part...
-# 
+class[Pos5] <- "Q5"
 
-setwd("~/Documentos/Omar/Tesis/Taxa/Results/Final2/")
+ClassAvTD1 <- class
 
-rawGrid <- read.csv("grid_1g.dist.matrix",header = T)
+class[Pos5] <- "Q5"
+class[Pos4] <- "Q4"
+class[Pos3] <- "Q3"
+class[Pos2] <- "Q2"
+class[NonVal] <- "NO"
 
-sp <- rawGrid$especie
+ClassAvTD2 <- class
 
-rawGrid <- rawGrid[,-1]
-rownames(rawGrid) <- sp
-
-rawGrid <- t(rawGrid)
-
-out.cells <- c()
-
-sp.len <- length(rawGrid[1,])
-
-for(i in 1:length(rownames(rawGrid))){
-  
-  len.c <- length(grep(0,rawGrid[i,]))
-  
-  if(len.c==sp.len){
-    
-    out.cells <- c(out.cells,rownames(rawGrid)[i])
-    
-  }
-  
-}
-
-
-rawGrid <- rawGrid[-which(rownames(rawGrid)%in%out.cells),]
-
-dist2 <- as.matrix(vegdist(rawGrid,method = 'jaccard',binary = T,na.rm = T))
-
-dist2 <- dist2[which(rownames(dist2)%in%area[grep("Q5",class)]), ]
-
-brks2 <- c(0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1)
-
-for(i in 1:length(rownames(dist2))){
-  
-  class.v <- findInterval(dist2[i,], brks2, all.inside = T)
-  
-  dist2[i, ] <- class.v
-}
-
-dist2
-
-CI.cells <- c()
-
-l2 <- length(dist2[,1])
-
-for (i in 1:length(colnames(dist2))){
-  
-  l1 <- length(grep(10,dist2[ ,i]))
-  
-  if(l1==l2){
-    CI.cells <- c(CI.cells,colnames(dist2)[i])
-  }
-  
-}
-
-dist.final <- dist2[,which(colnames(dist2)%in%CI.cells)]
-
-class[which(area%in%colnames(dist.final))]
-
-class[which(area%in%colnames(dist.final))] <- "CI.1"
-
-class
-
-grid$Index <- class
+grid$Index <- ClassAvTD2
 
 setwd("~/Documentos/Omar/Tesis/Taxa/Results/Final2/RawIndex_R/")
 
-writePolyShape(grid,fn = "Grid1_AvTD")
+writePolyShape(grid,fn = "Grid25_AvTD")
+
+########################################################
+########################################################
+
+## Go to the work directory where are the shape poly 
+setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Distribution/shp/Grid/")
+# Read the shape file
+grid <- readShapePoly("grid_25g.shp")
+#
+# Now, go to the directory where are the index results
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/Final2/RawIndex_R/")
+# Read the results depending on the cell size
+x <- read.csv("PD.grid25",header=T)
+# Extract the index values
+area <- read.csv("DT.grid25")$area 
+x <- x$PD
+# Check the values
+x
+# Due to there are many cell without index values (this is beacuse the species distribution)
+# remove the 0 values, or the quantile classification will be biased
+x2 <- x[which(x>0.0000000)]
+
+# No, do the quantile clasification, in this case, given the values index, the intervals will be generated
+brks <- classIntervals(x2,n=5,style = "quantile")
+brks <- brks$brks
+# Here, the intervarls given the index value
+brks
+
+# Now, each index values will be classified in a category given the quantile intervals
+# Here, the all index values (include those cells with index values equal 0)
+# Just, beacuase the final shape file neead a value for each cell
+class <- findInterval(x,brks,all.inside = T)
+# Now, beacuase the focus is only in the quantile 5 and 4, (those with highest index values)
+# The cells out of the Q5 and Q4 will be replace with NO
+Pos5 <- grep(5,class)
+Pos4 <- grep(4,class)
+Pos3 <- grep(3,class)
+Pos2 <- grep(2,class)
+NonVal <- which(x==0)
+
+class[-Pos5] <-  "Non-Q5"
+# Now, those cell int in the two last quantiles, will be filled with the quotes "Q5" and "Q4"
+class[Pos5] <- "Q5"
+
+ClassPD1 <- class
+
+class[Pos5] <- "Q5"
+class[Pos4] <- "Q4"
+class[Pos3] <- "Q3"
+class[Pos2] <- "Q2"
+class[NonVal] <- "NO"
+
+ClassPD2 <- class
+
+grid$Index <- ClassPD2
+
+setwd("~/Documentos/Omar/Tesis/Taxa/Results/Final2/RawIndex_R/")
+
+writePolyShape(grid,fn = "Grid25_PD")
+
+
+#####################################################
+#####################################################
+
+## Go to the work directory where are the shape poly 
+setwd("/home/omar/Documentos/Omar/Tesis/Scripts/Distribution/shp/Grid/")
+# Read the shape file
+grid <- readShapePoly("grid_25g.shp")
+#
+# Now, go to the directory where are the index results
+setwd("/home/omar/Documentos/Omar/Tesis/Taxa/Results/Final2/RawIndex_R/")
+# Read the results depending on the cell size
+x <- read.csv("DT.grid25",header=T)
+# Extract the index values
+area <- read.csv("DT.grid25")$area 
+x <- x$W
+# Check the values
+x
+# Due to there are many cell without index values (this is beacuse the species distribution)
+# remove the 0 values, or the quantile classification will be biased
+x2 <- x[which(x>0.0000000)]
+
+# No, do the quantile clasification, in this case, given the values index, the intervals will be generated
+brks <- classIntervals(x2,n=5,style = "quantile")
+brks <- brks$brks
+# Here, the intervarls given the index value
+brks
+
+# Now, each index values will be classified in a category given the quantile intervals
+# Here, the all index values (include those cells with index values equal 0)
+# Just, beacuase the final shape file neead a value for each cell
+class <- findInterval(x,brks,all.inside = T)
+# Now, beacuase the focus is only in the quantile 5 and 4, (those with highest index values)
+# The cells out of the Q5 and Q4 will be replace with NO
+Pos5 <- grep(5,class)
+Pos4 <- grep(4,class)
+Pos3 <- grep(3,class)
+Pos2 <- grep(2,class)
+NonVal <- which(x==0)
+
+class[-Pos5] <-  "Non-Q5"
+# Now, those cell int in the two last quantiles, will be filled with the quotes "Q5" and "Q4"
+class[Pos5] <- "Q5"
+
+ClassTD1 <- class
+
+class[Pos5] <- "Q5"
+class[Pos4] <- "Q4"
+class[Pos3] <- "Q3"
+class[Pos2] <- "Q2"
+class[NonVal] <- "NO"
+
+ClassTD2 <- class
+
+grid$Index <- ClassTD2
+
+setwd("~/Documentos/Omar/Tesis/Taxa/Results/Final2/RawIndex_R/")
+
+writePolyShape(grid,fn = "Grid25_AvTD")
+
+
+######################################################################
+######################################################################
+
+AllClass <- cbind(as.matrix(ClassAvTD1),
+                  as.matrix(ClassPD1),
+                  as.matrix(ClassTD1))
+
+setwd("~/Documentos/Omar/Tesis/Taxa/Results/Final2/RawIndex_R/")
+
+write.csv(AllClass, "AllIndices_Quantiles.csv", quote = F, row.names = F, col.names = T)
