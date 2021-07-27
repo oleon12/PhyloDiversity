29b5ba79c61bc2a368bd357fa0ba9bad877352f2
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2021-07-27 14:52:24 -0300

    Update Indices_PA.R

--- R/Indices_PA.R
+++ R/Indices_PA.R
@@ -1,3 +1,31 @@
+library(ape)
+library(phytools)
+library(vegan)
+library(maptools)
+library(RColorBrewer)
+library(classInt)
+library(maps)
+library(maptools)
+library(dismo)
+library(picante)
+library(rgdal)
+library(ggplot2)
+library(gridExtra)
+
+########################################################################
+
+setwd("../Data/Trees/")
+
+########################################################################
+
+source("....find.root.R")
+source("....match.phy.dist.R")
+source("....pd2.R")
+source("....toNum.R")
+
+########################################################################
+#                               Tree List                              #
+########################################################################
 ########################################################################
 #                               Tree List                              #
 ########################################################################
@@ -13,18 +41,8 @@ names(multi.phylo) <- dir.tree
 #                              Areas List                              #
 ########################################################################
 
-setwd("~/Documentos/Omar/Projects/Tesis2/Richness/")
-
-multi.data <- list(read.csv("PNN.inn_25g.matrix"),read.csv("PNN.out_25g.matrix"))
+multi.data <- list(read.csv("../Data/Distributions/PNN.inn_25g.matrix"),read.csv("../Data/Distributions/PNN.out_25g.matrix"))
 names(multi.data) <- c("PNN.in","PNN.out")
-
-########################################################################
-#                           IUCN Categories                            #
-########################################################################
-
-iucn <- read.csv("../Richness/IUCN_2.csv")
-
-iucn <- iucn[ , 1:2]
 
 ########################################################################
 #                   Phylogenetic Diversity Indices                     #
@@ -35,26 +53,7 @@ pd.out <- pd2(Phylogeny = multi.phylo, Distribution = multi.data, pdRoot = T)
 
 names(pd.out) <- names(multi.data)
 
-##AvTD
-avtd.out <- avtd2(Phylogeny = multi.phylo, Distribution = multi.data, avtdRoot = T)
-
-names(avtd.out) <- names(multi.data)
-
-##EDGE
-edge.r2 <- edge2(Phylo = multi.phylo, IUCN.cat = iucn, na.Cat = c("LC","NT"), freq.Val = c(0.5,0.5), n.iter = 30)
-
-edge.r2V <- edge.r2$Resampled.EDGE
-edge.r2V <- data.frame(Species = edge.r2V$Species, 
-                       EDGE = as.vector(apply(edge.r2V[,2:31], 1, mean)))
-
-edge.out <- edge.area(edge.val = edge.r2V, matrix = multi.data)
-
-names(edge.out) <- names(multi.data)
-
-
-save(pd.out, file = "../Indices/pd.out_PNN.rda")
-save(avtd.out, file = "../Indices/avtd.out_PNN.rda")
-save(edge.out, file = "../Indices/edge.out_PNN.rda")
+save(pd.out, file = "../Data/Indices/pd.out_PNN.rda")
 
 ###########################################################
 
@@ -63,20 +62,20 @@ PNN.in <- data.frame(Ricness = pd.out$PNN.in$SR,
                      AvTD = avtd.out$PNN.in$Dplus,
                      EDGE = apply(t(edge.out$PNN.in),1,sum))
 
-write.csv(PNN.in, "../Indices/Indices_PNN.in_Table.csv", quote = F, row.names = F)
+write.csv(PNN.in, "../Data/Indices/Indices_PNN.in_Table.csv", quote = F, row.names = F)
 
 PNN.out <- data.frame(Ricness = pd.out$PNN.out$SR,
                       PD = pd.out$PNN.out$PD,
                       AvTD = avtd.out$PNN.out$Dplus,
                       EDGE = apply(t(edge.out$PNN.out),1,sum))
 
-write.csv(PNN.out, "../Indices/Indices_PNN.out_Table.csv", quote = F, row.names = F)
+write.csv(PNN.out, "../Data/Indices/Indices_PNN.out_Table.csv", quote = F, row.names = F)
 
 ###########################################################
 
-inPNN <- read.csv("../Indices/Indices_PNN.in_Table.csv")
-outPNN <- read.csv("../Indices/Indices_PNN.out_Table.csv")
-Total <- read.csv("../Indices/Indices_Table.csv")
+inPNN <- read.csv("../Data/Indices/Indices_PNN.in_Table.csv")
+outPNN <- read.csv("../Data/Indices/Indices_PNN.out_Table.csv")
+Total <- read.csv("../Data/Indices/Indices_Table.csv")
 
 outTotal <- apply(outPNN, 2, sum)
 inTotal <- apply(inPNN, 2, sum)
@@ -94,4 +93,4 @@ OutTable <- as.data.frame(OutTable)
 
 OutTable
 
-write.csv(OutTable, "../Indices/PNN_Results.csv", quote = F, row.names = T)
+write.csv(OutTable, "../Data/Indices/PNN_Results.csv", quote = F, row.names = T)
